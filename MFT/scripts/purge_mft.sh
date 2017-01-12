3f04d2c9dd1a19048d02f76291d87975c508ddb5
U-HNAMAIZA-JP\HNAMAIZA <HNAMAIZA@HNAMAIZA-JP.jp.oracle.com>
2017-01-12 22:12:32 +0900

    MFT֘A̃XNvgt@Cł·B

---
+++ MFT/scripts/purge_mft.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export ORACLE_HOME=/home/oracle/Oracle/Middleware/Oracle_Home
+export WLST_SCRIPT=/home/oracle/purge_mft.py
+
+${ORACLE_HOME}/mft/common/bin/wlst.sh ${WLST_SCRIPT}
+
+exit $?
