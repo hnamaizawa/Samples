b6eb82ad4230945023f1ed66446d14d2423aace6
hnamaizawa <hnamaizawa@gmail.com>
2017-01-12 22:15:49 +0900

    Update purge_mft.py
    削除期間の開始を2017年、終了を2020年としました。

--- MFT/scripts/purge_mft.py
+++ MFT/scripts/purge_mft.py
@@ -3,8 +3,8 @@ print 'Purge Instance and Payload.\n'
 username   = 'weblogic'
 password   = 'welcome1'
 url        = 't3://demo.jp.oracle.com:8001'
-start_date = '01-01-2014 00:00:00:00'
-end_date   = '31-12-2014 00:00:00:00'
+start_date = '01-01-2017 00:00:00:00'
+end_date   = '31-12-2020 00:00:00:00'
 
 connect(username,password,url)
 purgeInstanceData(start_date, end_date, testMode='false', status='*', runPayloadPurge='TRUE')
